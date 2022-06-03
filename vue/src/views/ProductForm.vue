8e08a067f714739654255038e4e3b0349bb8c38c
Iman <imanali.cse@gmail.com>
2022-06-03 07:47:07 +0600

    image in vue

--- vue/src/views/ProductForm.vue
+++ vue/src/views/ProductForm.vue
@@ -29,11 +29,12 @@
               <div>
                 <label class="block text-sm text-left font-medium text-gray-700"> Image </label>
                 <div class="mt-1 flex items-center">
-                  <img v-if="model.image" :src="model.image" :alt="model.name" class="w-64 h48 object-cover" />
+                  <img v-if="model.image" :src="model.image" :alt="model.name" class="w-64 h48 object-cover"/>
                   <span v-else class="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                    <svg class="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-                      <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                    </svg>
+                    <img class="w-64 h48 object-cover"  id="product_image"/>
+<!--                    <svg class="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">-->
+<!--                      <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />-->
+<!--                    </svg>-->
                   </span>
                   <button type="button" class="relative ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Change
@@ -87,6 +88,7 @@ export default {
               price: '',
               image: null
             },
+            p_image: null,
             router: useRouter()
         }
     },
@@ -104,7 +106,14 @@ export default {
     },
     methods: {
         onChangeFile(event) {
-           this.model.image = event.target.files[0];
+            let image = event.target.files[0];
+            let reader = new FileReader();
+            reader.onloadend = function() {
+              document.getElementById("product_image").src = reader.result;
+            }
+            reader.readAsDataURL(image);
+
+            this.p_image = image;
         },
         saveProduct(ev) {
           document.querySelectorAll(".validate_error").forEach(ele =>{
@@ -112,7 +121,7 @@ export default {
           });
 
           let fd = new FormData(document.getElementById('productForm'));
-          fd.append('image', this.model.image);
+          fd.append('image', this.p_image);
           for (const keyValue of fd) {
             console.log(keyValue[0] + ': ',  keyValue[1]);
           }
